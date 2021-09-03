7be22f65d449c4461625d0d2c3834a5a327bb6fa
Evan Uhlmeyer <evanuhl10@hotmail.com>
2021-09-02 21:16:08 -0500

    Create basic post/get request app

---
+++ Experiments/Evan_Uhlmeyer_Basic_Request/src/main/java/com/exp2/application/test/Data.java
@@ -0,0 +1,17 @@
+package com.exp2.application.test;
+
+public class Data {
+	
+	private String message;
+	
+	public Data() {}
+	
+	public Data(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+}
