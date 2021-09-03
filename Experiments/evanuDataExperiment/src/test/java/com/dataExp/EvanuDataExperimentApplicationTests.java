18897212f802ef0bf0b800f64cf49df394a6f577
Evan Uhlmeyer <evanuhl10@hotmail.com>
2021-09-03 15:16:29 -0500

    Experimenting with data, using objects

---
+++ Experiments/evanuDataExperiment/src/test/java/com/dataExp/EvanuDataExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataExp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvanuDataExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
