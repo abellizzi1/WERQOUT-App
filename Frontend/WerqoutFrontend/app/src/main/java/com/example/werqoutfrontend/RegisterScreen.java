0db9fbf1b8c65cc76820b7ea1b8a99030173a1a5
abellizzi1 <abelliz734@gmail.com>
2021-09-27 15:29:34 -0500

    added fields check

--- Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/RegisterScreen.java
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/RegisterScreen.java
@@ -2,11 +2,20 @@ package com.example.werqoutfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class RegisterScreen extends AppCompatActivity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,45 @@ public class RegisterScreen extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         userAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         userSpinner.setAdapter(userAdapter);
+
+
+        Button submitRegisterButton = findViewById(R.id.register_button_register);
+        EditText firstNameInput = findViewById(R.id.firstName_input_register);
+        EditText lastNameInput = findViewById(R.id.lastName_input_register);
+        EditText emailInput = findViewById(R.id.email_input_register);
+        EditText passwordInput = findViewById(R.id.password_input_register);
+
+        submitRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+            }
+        });
+
+    }
+
+    public boolean isValidEmail(String email)
+    {
+        String[] emailSplit = email.split("@");
+        if (emailSplit.length == 2 && isAlphaNumeric(emailSplit[0]))
+        {
+            String[] periodSplit = emailSplit[1].split(".");
+            if (periodSplit.length == 2 && isAlphaNumeric(periodSplit[0] + periodSplit[1]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isValidName(String name)
+    {
+        return name.matches("[a-zA-Z]+");
+    }
+
+    public boolean isAlphaNumeric(String s)
+    {
+        return s.matches("[a-zA-Z0-9]+");
     }
 }
