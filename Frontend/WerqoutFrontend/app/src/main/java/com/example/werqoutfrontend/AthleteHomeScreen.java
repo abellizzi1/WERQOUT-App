c40c96b8397dda2dafae89741a2c4a20d672c39d
abellizzi1 <abelliz734@gmail.com>
2021-10-02 21:01:31 -0500

    started athlete home screen

---
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/AthleteHomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.werqoutfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AthleteHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.athlete_home_screen);
+    }
+}
---
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/CoachHomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.werqoutfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoachHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coach_home_screen);
+    }
+}
