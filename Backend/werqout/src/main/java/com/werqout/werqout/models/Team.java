bd621ef55c4d63305c7519334c4ee1fc627bf944
Evan Uhlmeyer <evanuhl10@hotmail.com>
2021-10-28 13:36:21 -0500

    added onetoone to team model

--- Backend/werqout/src/main/java/com/werqout/werqout/models/Team.java
+++ Backend/werqout/src/main/java/com/werqout/werqout/models/Team.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.OneToOne;
+
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
