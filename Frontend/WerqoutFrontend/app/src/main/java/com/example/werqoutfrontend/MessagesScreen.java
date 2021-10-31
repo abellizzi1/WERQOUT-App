b2098118ea155b4890d550ba4aed8d3db2803d82
Colin Brenizer <cbren@iastate.edu>
2021-10-31 16:34:40 -0500

    Started working on message screen layout

---
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/MessagesScreen.java
@@ -0,0 +1,15 @@
+package com.example.werqoutfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessagesScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages_screen);
+
+    }
+}
