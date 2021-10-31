03672a4a22b2bb85292b9cefc6cf5d09d49e131d
Colin Brenizer <cbren@iastate.edu>
2021-10-31 16:03:43 -0500

    Cleaned up tests and recycler view components

--- Frontend/WerqoutFrontend/app/src/test/java/com/example/werqoutfrontend/UnitTest.java
+++ Frontend/WerqoutFrontend/app/src/test/java/com/example/werqoutfrontend/UnitTest.java
@@ -1,10 +1,12 @@
 package com.example.werqoutfrontend;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -19,21 +21,37 @@ import java.util.ArrayList;
 @RunWith(MockitoJUnitRunner.class)
 public class UnitTest {
 
+    private FilterResults filterResults;
+    private ArrayList<RecyclerViewComponent> searchResults;
+    private FilterLogic filterLogic;
+
+    private RecyclerViewComponent tim;
+    private RecyclerViewComponent tom;
+    private RecyclerViewComponent ramona;
+
+    @Before public void initialize()
+    {
+        filterResults = mock(FilterResults.class);
+        tim = new RecyclerViewComponent(R.drawable.ic_default_account_box,
+                "tim", "tim@gmail.com", 0);
+        tom = new RecyclerViewComponent(R.drawable.ic_default_account_box,
+                "tom", "tom@gmail.com", 1);
+        ramona = new RecyclerViewComponent(R.drawable.ic_default_account_box,
+                "Ramona", "ramona@gmail.com", 2);
+        searchResults = new ArrayList<>();
+        searchResults.add(tim);
+        searchResults.add(tom);
+        searchResults.add(ramona);
+        filterLogic = new FilterLogic();
+    }
+    /*
+    This first test simulates typing the letter t into the search bar. What should happen is that
+    the filter should return a list containing tim and tom, because ron does not have the letter t
+    in it.
+     */
     @Test
     public void testSearchFilter1()
     {
-        FilterResults filterResults = mock(FilterResults.class);
-        RecyclerViewComponent tim = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tim", "tim@gmail.com", 0);
-        RecyclerViewComponent tom = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tom", "tom@gmail.com", 1);
-        RecyclerViewComponent ron = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "Ron", "ron@gmail.com", 2);
-        ArrayList<RecyclerViewComponent> searchResults = new ArrayList<>();
-        searchResults.add(tim);
-        searchResults.add(tom);
-        searchResults.add(ron);
-        FilterLogic filterLogic = new FilterLogic();
         ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("t",searchResults);
 
         ArrayList<RecyclerViewComponent> filterTest1 = new ArrayList<>();
@@ -48,27 +66,20 @@ public class UnitTest {
             assertSame(simulatedResponse.get(i).getText1(), actualResponse.get(i).getText1());
         }
     }
+    /*
+    Test 2 simulates entering the letter m into the search query. Since every user has the letter
+    m in their username, the filter should return a list that contains every user.
+     */
     @Test
     public void testSearchFilter2()
     {
-        FilterResults filterResults = mock(FilterResults.class);
-        RecyclerViewComponent tim = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tim", "tim@gmail.com", 0);
-        RecyclerViewComponent tom = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tom", "tom@gmail.com", 1);
-        RecyclerViewComponent ron = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "Ron", "ron@gmail.com", 2);
-        ArrayList<RecyclerViewComponent> searchResults = new ArrayList<>();
-        searchResults.add(tim);
-        searchResults.add(tom);
-        searchResults.add(ron);
-        FilterLogic filterLogic = new FilterLogic();
-        ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("o",searchResults);
+        ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("m",searchResults);
 
         ArrayList<RecyclerViewComponent> filterTest1 = new ArrayList<>();
         ArrayList<RecyclerViewComponent> simulatedResponse = new ArrayList<>();
+        filterTest1.add(tim);
         filterTest1.add(tom);
-        filterTest1.add(ron);
+        filterTest1.add(ramona);
 
         when(filterResults.getFilteredList()).thenReturn(filterTest1);
         simulatedResponse.addAll(filterResults.getFilteredList());
@@ -77,26 +88,18 @@ public class UnitTest {
             assertSame(simulatedResponse.get(i).getText1(), actualResponse.get(i).getText1());
         }
     }
+    /*
+    This test simulates entering r into the search query. Since ramona is the only user that has
+    the letter r in their username, the filter should return a list that only contains ramona.
+     */
     @Test
     public void testSearchFilter3()
     {
-        FilterResults filterResults = mock(FilterResults.class);
-        RecyclerViewComponent tim = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tim", "tim@gmail.com", 0);
-        RecyclerViewComponent tom = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tom", "tom@gmail.com", 1);
-        RecyclerViewComponent ron = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "Ron", "ron@gmail.com", 2);
-        ArrayList<RecyclerViewComponent> searchResults = new ArrayList<>();
-        searchResults.add(tim);
-        searchResults.add(tom);
-        searchResults.add(ron);
-        FilterLogic filterLogic = new FilterLogic();
         ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("r",searchResults);
 
         ArrayList<RecyclerViewComponent> filterTest1 = new ArrayList<>();
         ArrayList<RecyclerViewComponent> simulatedResponse = new ArrayList<>();
-        filterTest1.add(ron);
+        filterTest1.add(ramona);
 
         when(filterResults.getFilteredList()).thenReturn(filterTest1);
         simulatedResponse.addAll(filterResults.getFilteredList());
@@ -105,31 +108,19 @@ public class UnitTest {
             assertSame(simulatedResponse.get(i).getText1(), actualResponse.get(i).getText1());
         }
     }
+    /*
+    This test simulates entering a username that does not exist. The filter should return an empty list.
+     */
     @Test
     public void testSearchFilter4()
     {
-        FilterResults filterResults = mock(FilterResults.class);
-        RecyclerViewComponent tim = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tim", "tim@gmail.com", 0);
-        RecyclerViewComponent tom = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "tom", "tom@gmail.com", 1);
-        RecyclerViewComponent ron = new RecyclerViewComponent(R.drawable.ic_default_account_box,
-                "Ron", "ron@gmail.com", 2);
-        ArrayList<RecyclerViewComponent> searchResults = new ArrayList<>();
-        searchResults.add(tim);
-        searchResults.add(tom);
-        searchResults.add(ron);
-        FilterLogic filterLogic = new FilterLogic();
-        ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("Colin",searchResults);
+        ArrayList<RecyclerViewComponent> actualResponse = filterLogic.performFiltering("zach",searchResults);
 
         ArrayList<RecyclerViewComponent> filterTest1 = new ArrayList<>();
         ArrayList<RecyclerViewComponent> simulatedResponse = new ArrayList<>();
 
         when(filterResults.getFilteredList()).thenReturn(filterTest1);
         simulatedResponse.addAll(filterResults.getFilteredList());
-        for(int i = 0; i < actualResponse.size(); i++)
-        {
-            assertSame(simulatedResponse.get(i).getText1(), actualResponse.get(i).getText1());
-        }
+        assertTrue(actualResponse.size() == simulatedResponse.size());
     }
 }
