240de68d290c1fadf56a0addc8333a49e9a5f5f0
Evan Uhlmeyer <evanuhl10@hotmail.com>
2021-11-11 12:57:06 -0600

    First code for websocket

---
+++ Backend/werqout/src/main/java/com/werqout/werqout/websockets/MessagingWebSocket.java
@@ -0,0 +1,45 @@
+package com.werqout.werqout.websockets;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ServerEndpoint("/message/{userName}")
+@Component
+public class MessagingWebSocket {
+	
+	@Autowired
+	private Map<Session, String> sessionUsernameMap = new Hashtable<>();
+	
+	@Autowired
+	private Map<String, Session> usernameSessionMap = new Hashtable<>();
+	
+	private final Logger logger = LoggerFactory.getLogger(MessagingWebSocket.class);
+	
+	@OnOpen
+	public void onOpen(Session session, @PathParam("userName") String username) 
+	throws IOException {
+		logger.info("Session opened");
+		
+		sessionUsernameMap.put(session, username);
+		usernameSessionMap.put(username, session);
+		
+		String message = "User: " + username + "is online";
+	}
+	
+	
+
+}
