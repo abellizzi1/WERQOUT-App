cd983d6a902b06e36a66cea38f21a23284696e91
abellizzi1 <abelliz734@gmail.com>
2021-11-22 21:40:57 -0600

    athlete mygroup screen started

---
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/AthleteMyGroupScreen.java
@@ -0,0 +1,14 @@
+package com.example.werqoutfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AthleteMyGroupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.athlete_my_group_screen);
+    }
+}
