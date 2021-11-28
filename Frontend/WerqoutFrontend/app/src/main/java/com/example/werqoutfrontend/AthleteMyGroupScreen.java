ee2c89fe82a42d84b1ed077a61cde7da58d1f331
abellizzi1 <abelliz734@gmail.com>
2021-11-27 18:55:30 -0600

    athlete can now add rating to a team

--- Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/AthleteMyGroupScreen.java
+++ Frontend/WerqoutFrontend/app/src/main/java/com/example/werqoutfrontend/AthleteMyGroupScreen.java
@@ -2,12 +2,22 @@ package com.example.werqoutfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.werqoutfrontend.network.ServerRequest;
+import com.example.werqoutfrontend.utils.Const;
+
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AthleteMyGroupScreen extends AppCompatActivity {
 
@@ -28,5 +38,50 @@ public class AthleteMyGroupScreen extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        Button backButton = findViewById(R.id.back_button_athlete_mygroup);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This onClick function directs the user to the AthleteHomeScreen Screen when the "Back" button
+             * is clicked.
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), AthleteHomeScreen.class));
+            }
+        });
+
+        Button submitRatingButton = findViewById(R.id.submit_button_athlete_mygroup);
+        submitRatingButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This onClick function posts the selected rating to the team's rating.
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                TextView errorMessage = findViewById(R.id.error_message_athlete_mygroup);
+                errorMessage.setText("");
+                String rating = userSpinner.getSelectedItem().toString();
+                if (rating.equals("Select rating"))
+                {
+                    errorMessage.setText("Invalid rating");
+                }
+                else
+                {
+                    int teamId = 0;
+                    try {
+                        teamId = (int)AthleteHomeScreen.getAthleteTeam().get("id");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Const.CURRENT_URL = "http://coms-309-034.cs.iastate.edu:8080/teams/" + teamId + "/rate?rating=" + rating;
+                    Map<String, String> params = new HashMap<>();
+                    params.put("rating", rating);
+                    ServerRequest ratingRequest = new ServerRequest();
+                    ratingRequest.jsonObjectRequest(Const.CURRENT_URL, 2, new JSONObject(params));
+                }
+            }
+        });
     }
 }
